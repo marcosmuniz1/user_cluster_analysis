0529cd919ff815056ede129da6ab3bec844c64b6
Marcos Muñiz <marcosmuniz93@gmail.com>
2023-10-17 19:08:14 +0200

    added clustering

--- cluster_analysis.py
+++ cluster_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from kmodes.kmodes import KModes
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import silhouette_score, davies_bouldin_score, calinski_harabasz_score
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -16,13 +17,14 @@ codes=pd.read_csv("Codebook.csv")
 ## ARE MORE OR LESS LIKELY TO BELONG TO A GIVEN CLUSTER
 
 
-print("dataset contains a couple of badly formatted binary columns then frequency indications for certain shopping behaviours\n")
+print("dataset contains a couple of badly formatted binary columns, then frequency indications for certain shopping behaviours\n")
 for c in df.columns:
     print(f"{c}:")
     print(df[c].unique())
     print("***")
     
 print("i will start with the binary stuff given that i can treat them as a group and work on them parallely")
+print("***\n")
 
 items=list(['A1','A2','A3','A4','A5','A6','A7','A8','A9','A10'])
 
@@ -51,20 +53,15 @@ for i in item_names:
 
 # i'd like to compare share of shoppers per category in a single chart
 
-shoppers=list()
-subset_size=len(df)
-
-for i in item_names:
-    shoppers.append(len(df[df[i]==True]))
-
-shoppers=np.array(shoppers)
-shoppers=shoppers/subset_size
-shoppers=pd.Series(dict(zip(item_names,shoppers)))
-
+shoppers = df[item_names].mean()
 shoppers.sort_values().plot(kind='bar')
-plt.show()
+plt.title("1) Share of people who made purchase of select products in last 30 days")
+plt.figure(figsize=(8, 6))
+plt.savefig('share_purchasers_overall.png') 
 
-print("A1 to A4 seem to be nonrecurrent and therefore uninformative for recurrent purchase behaviour so i'll take them out")
+print("notice: saved a chart with share of purchasers per category overall")
+print("A1 to A4 seem to be nonrecurrent and therefore uninformative for recurrent purchase behaviour so i took them out")
+print("***\n")
 
 item_names=item_names[4:]
 
@@ -86,23 +83,31 @@ for item1 in item_names:
                 
 plt.figure(figsize=(8, 6))
 sns.heatmap(overlaps, annot=True, cmap='YlGnBu', vmin=0, vmax=1)
-plt.show()
-
+plt.title("2) Share of column purchasers who also purchased row")
+plt.savefig('purchase_interactions.png')
+print("notice: saved a heatmap with item-to-item purchase interactions")
+print("***\n")
 
 # Custering #################
 
+print("clusters may help us have a better grasp on how purchase patterns between categories are intertwined")
+print("used KModes, will test different n_cluster configurations but init and n_init will remain fixed at Huang and =5")
+
 df_binary = df[item_names]
+#let's test different cluster n choices via goodnes of cluster metrics
 n_clusters=[2,3,4]
+# empty df for results
 results_df = pd.DataFrame(columns=
                           ['Number of Clusters', 'Silhouette Score', 'Davies-Bouldin Index', 'Calinski-Harabasz Index'])
 
+# loop through configurations and calculate appropiate metrics
 for n in n_clusters:
     km = KModes(n_clusters=n, init='Huang', n_init=5, verbose=0)
     clusters = km.fit_predict(df_binary)
     
-    silhouette_avg = silhouette_score(df_binary, clusters)
-    db_score = davies_bouldin_score(df_binary, clusters)
-    ch_score = calinski_harabasz_score(df_binary, clusters)
+    silhouette_avg = silhouette_score(df_binary, clusters) # how similar an object is to its own cluster (cohesion) compared to other clusters 
+    db_score = davies_bouldin_score(df_binary, clusters) # average similarity between each cluster and its most similar cluster
+    ch_score = calinski_harabasz_score(df_binary, clusters) #ratio of the between-cluster variance to within-cluster variance
     
     # Append results to the DataFrame
     results_df = results_df.append({
@@ -111,3 +116,49 @@ for n in n_clusters:
         'Davies-Bouldin Index': db_score,
         'Calinski-Harabasz Index': ch_score
     }, ignore_index=True)
+    
+    
+# I need to normalize to make feasible comparison between the metrics, ill use minmax for high=good and reverse the
+# minmax when the opposite is true")
+
+results_df_altered=results_df.copy()
+
+# high=good
+scaler = MinMaxScaler()
+columns_to_scale = ['Silhouette Score', 'Calinski-Harabasz Index']
+scaled_columns = scaler.fit_transform(results_df_altered[columns_to_scale])
+results_df_altered[columns_to_scale] = scaled_columns
+
+# high=bad
+Max_dbi = results_df_altered['Davies-Bouldin Index'].max()
+Min_dbi = results_df_altered['Davies-Bouldin Index'].min()
+
+results_df_altered['Davies-Bouldin Index'] = 1 - (results_df_altered['Davies-Bouldin Index'] - Min_dbi) / (Max_dbi - Min_dbi)
+
+
+results_df_altered['Overall']=(results_df_altered['Silhouette Score']+
+                                       results_df_altered['Calinski-Harabasz Index']+
+                                       results_df_altered['Davies-Bouldin Index'])/3
+
+# create an overall measure to make a decision
+
+n_clusters=results_df_altered[results_df_altered['Overall']==results_df_altered['Overall'].max()]['Number of Clusters'].mean()
+print(f"{n_clusters} is the preferred cluster size based on cluster optimization\n")
+
+print(" i saw the results for 4 clusters and it wasnt very insighful, plus 4 categories is a bit too many, so i reduced to 3\n")
+km = KModes(n_clusters=3, init='Huang', n_init=10, verbose=0)
+clusters = km.fit_predict(df_binary)
+df['cluster']=clusters
+
+print("cluster sizes")
+print(df['cluster'].value_counts())
+
+shoppers = df.groupby(by='cluster')[item_names].mean()
+shoppers.plot(kind='bar', stacked=False)
+plt.title("3) Share of purchasers by cluster")
+plt.savefig('purchasers_by_cluster.png')
+
+print("notice: saved chart with how many people in each cluster are buying each item")
+print("***\n")
+
+print("the three cluster model that nonbasic purchases such as clothing or alcohol define belonging to a subset of consumers. clusters 0 and 1 are somewhat more proximate, with gasoline being a key differentiator")
