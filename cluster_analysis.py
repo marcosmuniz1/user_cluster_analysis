53ba1fa9f2614a8d0bbf9fd58eecc18b6c339a2b
Marcos Muñiz <marcosmuniz93@gmail.com>
2023-10-18 17:54:41 +0200

    added clustering analysis for channel usage

--- cluster_analysis.py
+++ cluster_analysis.py
@@ -102,7 +102,7 @@ results_df = pd.DataFrame(columns=
 
 # loop through configurations and calculate appropiate metrics
 for n in n_clusters:
-    km = KModes(n_clusters=n, init='Huang', n_init=5, verbose=0)
+    km = KModes(n_clusters=n, init='Huang', n_init=10, verbose=0)
     clusters = km.fit_predict(df_binary)
     
     silhouette_avg = silhouette_score(df_binary, clusters) # how similar an object is to its own cluster (cohesion) compared to other clusters 
@@ -145,20 +145,192 @@ results_df_altered['Overall']=(results_df_altered['Silhouette Score']+
 n_clusters=results_df_altered[results_df_altered['Overall']==results_df_altered['Overall'].max()]['Number of Clusters'].mean()
 print(f"{n_clusters} is the preferred cluster size based on cluster optimization\n")
 
-print(" i saw the results for 4 clusters and it wasnt very insighful, plus 4 categories is a bit too many, so i reduced to 3\n")
-km = KModes(n_clusters=3, init='Huang', n_init=10, verbose=0)
+
+km = KModes(n_clusters=4, init='Huang', n_init=10, verbose=0)
 clusters = km.fit_predict(df_binary)
 df['cluster']=clusters
 
-print("cluster sizes")
+print(f"cluster sizes for {n_clusters} splits")
 print(df['cluster'].value_counts())
 
 shoppers = df.groupby(by='cluster')[item_names].mean()
 shoppers.plot(kind='bar', stacked=False)
-plt.title("3) Share of purchasers by cluster")
-plt.savefig('purchasers_by_cluster.png')
+plt.title("3) Share of purchasers by cluster - 4 splits")
+plt.savefig('purchasers_by_cluster4.png')
 
 print("notice: saved chart with how many people in each cluster are buying each item")
 print("***\n")
 
-print("the three cluster model that nonbasic purchases such as clothing or alcohol define belonging to a subset of consumers. clusters 0 and 1 are somewhat more proximate, with gasoline being a key differentiator")
+print("looks like there is a progression in the number of purchase types by cluster, with the note that a relevant chunk buys few stuff but still fuel ")
+print("this could be interesting if i had to analyze strategies to reach underserved consumers")
+print("apart from that, i'd think a scale of how many item categories they spent in through the last month would be clearer to communicate\n***\n")
+#print("the three cluster model that nonbasic purchases such as clothing or alcohol define belonging to a subset of consumers. clusters 0 and 1 are somewhat more proximate, with gasoline being a key differentiator")
+
+print("let's see if there are implicit intertwines in channel usage\n")
+print("step one is to see how frequent is each channel\n")
+
+channel_u_columns = ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7']
+channel_mapping= {
+    'B1':"In-store",
+    'B2':"Online with delivery",
+    'B3':"Curbside",
+    'B4':"Pickup",
+    'B5':"Subscription",
+    'B6':"Same-day app",
+    'B7':"Marketplace"
+}
+
+df.rename(columns=channel_mapping, inplace=True)
+channel_u_columns=df.columns[-11:-4].values
+
+# Define the factor ordering and convert columns to ordered categorical data
+factor_ordering = [
+    "I have not done this in the last 30 days.",
+    "Not weekly but monthly",
+    "Not daily but weekly",
+    "Daily"
+]
+
+for column in channel_u_columns:
+    df[column] = pd.Categorical(df[column], categories=factor_ordering, ordered=True)
+    
+n_levels = len(factor_ordering)
+color_palette = sns.color_palette("YlGnBu", n_levels)
+
+# Create a figure with subplots for each column
+fig, axes = plt.subplots(1, len(channel_u_columns), figsize=(15, 5),sharey=True)
+
+# Iterate through columns and plot the relative frequencies with color gradients
+for i, column in enumerate(channel_u_columns):
+    # Calculate relative frequencies
+    relative_freq = df[column].value_counts(normalize=True).loc[factor_ordering]
+    
+    # Plot a bar chart with color gradients
+    sns.barplot(x=relative_freq.index, y=relative_freq.values, ax=axes[i], palette=color_palette)
+    axes[i].set_title(column)
+    axes[i].set_ylabel("Relative Frequency")
+    axes[i].set_xticklabels([])
+
+plt.suptitle("Usage Frequency, by Channel")
+plt.tight_layout()
+plt.savefig('channel_usage_freqs.png')
+print("note: saved a chart with univariate frequency distributions. most channels are rare except in-store and online with delivery ")
+print("this suggests two or three groups may be enough to differentiate the consumer base, but let's see ")
+print("on to building the clusters")
+
+#there are potentially three possible mappings for these factors, let's start with number 3 and then we can test the others
+
+# basic encoding with not done=0
+factor_mapping1 = {
+    "I have not done this in the last 30 days.":0,
+    "Not weekly but monthly":1,
+    "Not daily but weekly":2,
+    "Daily":3
+}
+
+# # basic encoding as if we had no domain knowledge
+factor_mapping2 = {
+    "I have not done this in the last 30 days.":1,
+    "Not weekly but monthly":2,
+    "Not daily but weekly":3,
+    "Daily":4
+}
+
+#based on "days a month"
+factor_mapping3= {
+    "I have not done this in the last 30 days.":0,
+    "Not weekly but monthly":1,
+    "Not daily but weekly":4,
+    "Daily":30
+}
+
+print("aside from the mapping, ill swap nans for zeros (if they do not answer the question i'ts because they purchased nothing)")
+mapped_df = df[channel_u_columns].applymap(factor_mapping3.get).fillna(0)
+
+print(" based on the first glance at the data i'll try a 3 cluster model\n")
+
+km = KModes(n_clusters=3, init='Huang', n_init=10, verbose=0)
+clusters = km.fit_predict(mapped_df)
+mapped_df['cluster']=clusters
+
+print(f"cluster sizes for 3 splits")
+print(mapped_df['cluster'].value_counts())
+
+#revert the mapping so its clearer for visualization
+labels_df=df[channel_u_columns].copy().fillna("I have not done this in the last 30 days.")
+labels_df['cluster']=mapped_df['cluster'].copy()
+
+# Get unique clusters
+unique_clusters = labels_df['cluster'].unique()
+
+# Create a figure with subplots for each cluster and each column
+fig, axes = plt.subplots(len(unique_clusters), len(labels_df.columns) - 1, figsize=(15, 5))
+
+# Iterate through clusters
+for i, cluster in enumerate(unique_clusters):
+    cluster_df = labels_df[labels_df['cluster'] == cluster]
+    
+    # Iterate through columns and plot the relative frequencies with color gradients for each cluster
+    for j, column in enumerate(cluster_df.columns[:-1]):  # Exclude the 'cluster' column
+        ax = axes[i, j]
+        relative_freq = cluster_df[column].value_counts(normalize=True).loc[factor_ordering]
+        sns.barplot(x=relative_freq.index, y=relative_freq.values, ax=ax, palette=color_palette)
+        ax.set_title(f"Cluster {cluster}, {column}")
+        ax.set_xticklabels([])
+
+# Add a common y-axis label
+for ax in axes[:, 0]:
+    ax.set_ylabel("Relative Frequency")
+
+# Add a chart title
+plt.suptitle("Factor Frequencies by Column per Cluster")
+
+# Adjust layout and display the plot
+plt.tight_layout()
+plt.show()
+
+print("intuition behind clusters is pretty straight forward: mainly in-store shoppers, those who have incorporated online ordering, and those who use multiple channels")
+
+print(" can't jurt to see how it looks with 4 clusters\n")
+
+km = KModes(n_clusters=4, init='Huang', n_init=10, verbose=0)
+clusters = km.fit_predict(mapped_df)
+mapped_df['cluster']=clusters
+
+print(f"cluster sizes for 4 splits")
+print(mapped_df['cluster'].value_counts())
+
+#revert the mapping so its clearer for visualization
+labels_df=df[channel_u_columns].copy().fillna("I have not done this in the last 30 days.")
+labels_df['cluster']=mapped_df['cluster'].copy()
+
+# Get unique clusters
+unique_clusters = labels_df['cluster'].unique()
+
+# Create a figure with subplots for each cluster and each column
+fig, axes = plt.subplots(len(unique_clusters), len(labels_df.columns) - 1, figsize=(15, 5))
+
+# Iterate through clusters
+for i, cluster in enumerate(unique_clusters):
+    cluster_df = labels_df[labels_df['cluster'] == cluster]
+    
+    # Iterate through columns and plot the relative frequencies with color gradients for each cluster
+    for j, column in enumerate(cluster_df.columns[:-1]):  # Exclude the 'cluster' column
+        ax = axes[i, j]
+        relative_freq = cluster_df[column].value_counts(normalize=True).loc[factor_ordering]
+        sns.barplot(x=relative_freq.index, y=relative_freq.values, ax=ax, palette=color_palette)
+        ax.set_title(f"Cluster {cluster}, {column}")
+        ax.set_xticklabels([])
+
+# Add a common y-axis label
+for ax in axes[:, 0]:
+    ax.set_ylabel("Relative Frequency")
+
+# Add a chart title
+plt.suptitle("Factor Frequencies by Column per Cluster")
+
+# Adjust layout and display the plot
+plt.tight_layout()
+plt.show()
+
+#pretty much same intuition, only that new cluster uses the channels with more frequency
